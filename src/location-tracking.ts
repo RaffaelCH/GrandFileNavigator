0dc156b23433488716a0cf23f9a8fa5da15a5dc3
rafb <raffael.botschen@zuehlke.com>
2024-09-13 15:47:11 +0200

    Add view provider, update packages.

--- src/location-tracking.ts
+++ src/location-tracking.ts
@@ -179,7 +179,9 @@ function isObject(value: any): boolean {
   return typeof value === "object" && value !== null && !Array.isArray(value);
 }
 
-export function categorizePositionsByFileName(): { [fileName: string]: number } {
+export function categorizePositionsByFileName(): {
+  [fileName: string]: number;
+} {
   const positionHistory = getPositionHistory();
   const fileCountMap: { [fileName: string]: number } = {};
 
