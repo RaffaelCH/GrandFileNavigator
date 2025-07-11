c0b852657ee413c144efa42bf78e82e5fb707330
RaffaelCH <raffael.botschen@bluewin.ch>
2025-07-11 15:24:53 +0200

    sidebar ranges

--- interaction_analysis/plotting.py
+++ interaction_analysis/plotting.py
@@ -4,11 +4,11 @@ from helpers import remove_micronavigations
 
 step_size = 100  # plot step size in ms
 
-interaction_types = ["Scroll", "ChangeVisibleRanges", "ChangeFile",
-                     "EditFile", "EditingSession", "NavigationJump", "ChangeSidebarVisibility"]
+relevant_interaction_types = ["Scroll", "ChangeVisibleRanges", "ChangeFile",
+                              "EditFile", "EditingSession", "NavigationJump", "SidebarVisible"]
 color_map = matplotlib.colormaps['tab10']  # Use a standard colormap
-type_to_color = {itype: color_map(i / len(interaction_types))
-                 for i, itype in enumerate(sorted(interaction_types))}
+type_to_color = {itype: color_map(i / len(relevant_interaction_types))
+                 for i, itype in enumerate(sorted(relevant_interaction_types))}
 
 
 # TODO: Convert sidebar visibility to duration to use them as indicators.
@@ -31,6 +31,33 @@ def preprocess_interactions(interactions):
                 interactions_start
 
     return interactions
+
+
+# Toggling sidebar results in singular interactions -> convert them to time ranges where sidebar is visible
+def convert_sidebar_toggles_to_range(interactions):
+    sidebar_visibility_indicators = []
+    sidebar_toggles = [
+        interaction for interaction in interactions if interaction['interactionType'] == "ChangeSidebarVisibility"]
+
+    sidebar_visible = True  # sidebar is initially visible
+    visible_start = interactions[0]["timeStamp"]
+    for sidebar_toggle in sidebar_toggles:
+        if not sidebar_toggle["isVisible"]:
+            sidebar_visibility_indicators.append(
+                {"interactionType": "SidebarVisible", "startTime": visible_start, "endTime": sidebar_toggle["timeStamp"]})
+            sidebar_visible = False
+        else:
+            sidebar_visible = True
+            visible_start = sidebar_toggle["timeStamp"]
+
+    # Sidebar was visible at the end.
+    if sidebar_visible:
+        sidebar_visibility_indicators.append(
+            {"interactionType": "SidebarVisible", "startTime": visible_start, "endTime": interactions[-1]["timeStamp"]})
+
+    filtered_interactions = list(filter(
+        lambda interaction: interaction["interactionType"] != "ChangeSidebarVisibility", interactions))
+    return filtered_interactions + sidebar_visibility_indicators
 
 
 def convert_interactions(interactions):
@@ -56,14 +83,15 @@ def convert_interactions(interactions):
 
 def plot_interactions(interactions):
     interactions = preprocess_interactions(interactions)
+    sidebar_range_interactions = convert_sidebar_toggles_to_range(interactions)
     point_interactions, duration_interactions = convert_interactions(
-        interactions)
+        sidebar_range_interactions)
 
     # Create plot
     _, ax = plt.subplots(figsize=(15, 5))
 
     # Plot point interactions
-    for i_type in interaction_types:
+    for i_type in relevant_interaction_types:
         times = [p['time'] for p in point_interactions if p['type'] == i_type]
         ax.scatter(
             times,
@@ -75,7 +103,7 @@ def plot_interactions(interactions):
         )
 
     # Plot duration interactions
-    for i_type in interaction_types:
+    for i_type in relevant_interaction_types:
         relevant = [d for d in duration_interactions if d['type'] == i_type]
         for i, d in enumerate(relevant):
             start, end = d['start'], d['end']
